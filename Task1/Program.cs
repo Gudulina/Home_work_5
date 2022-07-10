3da783aaac77f55e811bb70de7d9ef5ffa3b7c07
Elena <gudulina@mail.ru>
2022-07-10 15:32:54 +0700

    Add task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,19 +2,18 @@
 Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами.
 Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2
-*/ 
+*/
 
 int[] Array()
-{   
+{
     int size = 8;
     int[] arr = new int[size];
     for (int i = 0; i < size; i++)
     {
-        arr[i] = new Random().Next(100, 1000);        
-    }    
+        arr[i] = new Random().Next(100, 1000);
+    }
     return arr;
 }
-
 
 void Print(int[] arr)
 {
@@ -26,8 +25,22 @@ void Print(int[] arr)
     Console.WriteLine();
 }
 
-int[] arr_1 = Array();
-Print(arr_1);
+void Number(int[] arr)
+{
+    int size = arr.Length;
+    int temp = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] % 2 == 0)
+            temp += 1;
+    }
+    Console.WriteLine($"Количество чётных числе: {temp}");
+}
 
-int[] arr_2 = Array();
-Print(arr_2);
+int[] arr1 = Array();
+Print(arr1);
+Number(arr1);
+
+int[] arr2 = Array();
+Print(arr2);
+Number(arr2);
